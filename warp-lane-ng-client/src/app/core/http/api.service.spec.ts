9092cacac42566a920e03c999befb084dd62679c
Lacicoco <lacicocodes@gmail.com>
2020-09-24 12:14:30 +0100

    unit test fixes

--- warp-lane-ng-client/src/app/core/http/api.service.spec.ts
+++ warp-lane-ng-client/src/app/core/http/api.service.spec.ts
@@ -1,12 +1,14 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing';
-
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ApiService);
   });
 
--- warp-lane-ng-client/src/app/file-converter/file-upload-button/file-upload-button.component.spec.ts
+++ warp-lane-ng-client/src/app/file-converter/file-upload-button/file-upload-button.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileUploadButtonComponent } from './file-upload-button.component';
@@ -8,6 +9,7 @@ describe('FileUploadButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ FileUploadButtonComponent ]
     })
     .compileComponents();
