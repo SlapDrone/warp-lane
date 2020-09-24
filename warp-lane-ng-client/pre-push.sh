65b70a5fc48a406eb20c2cb7d6ebc455900379ba
Lacicoco <lacicocodes@gmail.com>
2020-09-24 07:16:02 +0100

    Modified git hook scripts.

--- warp-lane-ng-client/pre-push.sh
+++ warp-lane-ng-client/pre-push.sh
@@ -1,7 +1,8 @@
 SRC_PATTERN="warp-lane-ng-client"
 if git diff --cached --name-only | grep --quiet "$SRC_PATTERN"
 then
+  ng e2e && ng test --watch=false && ng build --aot true
+fi
   echo "none"
   exit 0
-fi
-  ng e2e && ng test --watch=false && ng build --aot true
+  
