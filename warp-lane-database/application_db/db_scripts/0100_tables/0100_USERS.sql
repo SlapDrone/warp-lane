cd2a0a0144a1d88254bae6dfc635def8e36934ff
Lacico <Lacicocodes@gmail.com>
2021-01-03 12:52:46 +0000

    Added a sessions sql table.

--- warp-lane-database/application_db/db_scripts/0100_tables/0100_USERS.sql
+++ warp-lane-database/application_db/db_scripts/0100_tables/0100_USERS.sql
@@ -1,10 +1,18 @@
-CREATE TABLE public.USERS
+-- Table: public.users
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users
 (
     "USERID" integer NOT NULL,
     "USERNAME" text COLLATE pg_catalog."default",
     "PASSWORD" text COLLATE pg_catalog."default",
     "EMAILADDRESS" text COLLATE pg_catalog."default",
     "DATEMODIFIED" date,
-    CONSTRAINT USERS_PK PRIMARY KEY ("USERID")
-);
+    CONSTRAINT users_pk PRIMARY KEY ("USERID")
+)
 
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to admin;
