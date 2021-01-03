cf31b030148501db9961b2ba543d50c740b6654a
Lacico <Lacicocodes@gmail.com>
2021-01-03 20:44:03 +0000

    login now returns the sessionid

--- warp-lane-server/managers/session_manager.py
+++ warp-lane-server/managers/session_manager.py
@@ -1,21 +1,30 @@
 from dal.dal import DAL
-from dateutil import parser
 from datetime import datetime
 
 dal = DAL()
 
 
 def check_session(sessionid):
+    success = False
     sql_query = f'SELECT "EXPIRYTIME" FROM SESSIONS WHERE "SESSIONID"={sessionid}'
     result = dal.run_sql(sql_query)
     if len(result) > 0:
         expirytime = result[0][0]
         if expirytime > datetime.utcnow():
             update_session(sessionid)
-            return True
+            success = True
         else:
             delete_session(sessionid)
-    return False
+    return success
+
+
+def get_sessionid_for_userid(userid):
+    sql_query = f'SELECT "SESSIONID" FROM SESSIONS WHERE "USERID"={userid}'
+    result = dal.run_sql(sql_query)
+    if len(result) > 0:
+        return result[0][0]
+    else:
+        return None
 
 
 def delete_session(sessionid):
@@ -25,7 +34,7 @@ def delete_session(sessionid):
 
 
 def create_session(userid):
-    sql_query = f'INSERT INTO SESSIONS ("USERID", "EXPIRYTIME") VALUES ({int(userid)}, current_timestamp + (20 * interval \'1 minute\'))'
+    sql_query = f'INSERT INTO SESSIONS ("USERID", "EXPIRYTIME") VALUES ({int(userid)}, current_timestamp + (20 * interval \'1 minute\'));'
     sql_result = dal.run_sql(sql_query)
 
 
@@ -35,4 +44,4 @@ def update_session(sessionid):
 
 
 if __name__ == "__main__":
-    check_session(4)
+    val = check_session(1)
--- warp-lane-server/managers/user_manager.py
+++ warp-lane-server/managers/user_manager.py
@@ -1,22 +1,27 @@
 from dal.dal import DAL
-from managers.session_manager import create_session
+from managers.session_manager import create_session, get_sessionid_for_userid
 from utils.password_encryptor import encrypt_password, check_password
 
 dal = DAL()
 
 
 def login(username, unencrypted_password):
+    sessionid = None
     sql_query = (
         'SELECT "USERID", "PASSWORD" FROM USERS WHERE "USERNAME"=%s;'
     )
     sql_result = dal.run_sql(sql_query, [username])
-    user_id, encrypted_password = sql_result[0]
+    userid, encrypted_password = sql_result[0]
     valid = check_password(unencrypted_password, encrypted_password.encode("utf8"))
 
     if valid:
-        create_session(user_id)
-    return valid
+        sessionid = get_sessionid_for_userid(userid)
+        if not sessionid:
+            create_session(userid)
+            sessionid = get_sessionid_for_userid(userid)
+    return sessionid
 
 
 if __name__ == "__main__":
-    login("admin", "secret")
+    sessionid = login("admin", "secret")
+    print(sessionid)
